75d87bc188965c2a8413065ae7ed6cceb9be11e4
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-01 09:40:25 -0300

    finalizado projeto pokemon

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const fetchPokemon = () =>{
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    const pokemonPromises = []
 
-    const pokemonPromisses = [];
-
-    for (let i = 1; i < 150; i++) {
-        pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    for (let i = 1; i < 130; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     }
 
-    Promise.all(pokemonPromisses)
+    Promise.all(pokemonPromises)
     .then(pokemons => {
-        //console.log(pokemons)
+        console.log(pokemons)
 
         const lisPokemons = pokemons.reduce((acumulator, pokemon) =>{
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
             acumulator += `
-            <li class="card">
-            ${pokemon.name} 
+            <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+            <h2 class="card-title"> ${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')} </p>
+            
             </li>
             `
 
             return acumulator
-        },'')
+        }, '')
 
-        console.log(lisPokemons)
+        const ul = document.querySelector('[data-js="pokedex"]')
+
+        ul.innerHTML = lisPokemons
     })
 
 }
